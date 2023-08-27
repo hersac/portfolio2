100b04e85cecca4900cf68b7ff75bbd4ff33f581
Heriberto <heriberto@viumail.com>
2023-08-26 21:43:08 -0500

    Se modifica la estructura del proyecto

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", ()=>{
     const headerPlaceholder = document.getElementById("header");
+    const homePlaceholder = document.getElementById("home");
+    const mePlaceholder = document.getElementById("me");
+    const contactPlaceholder = document.getElementById("contact");
     const footerPlaceholder = document.getElementById("footer");
 
     // Cargar el contenido del header
@@ -9,10 +12,31 @@ document.addEventListener("DOMContentLoaded", function() {
             headerPlaceholder.innerHTML = data;
         });
 
+    // Cargar el contenido del Home
+    fetch("pages/home.html")
+        .then(response => response.text())
+        .then(data => {
+            homePlaceholder.innerHTML = data;
+    });
+
+    // Cargar el contenido del Me
+    fetch("pages/me.html")
+        .then(response => response.text())
+        .then(data => {
+            mePlaceholder.innerHTML += data;
+    });
+
+    // Cargar el contenido del Contact
+    fetch("pages/contact.html")
+        .then(response => response.text())
+        .then(data => {
+            contactPlaceholder.innerHTML = data;
+    });
+
     // Cargar el contenido del footer
     fetch("templates/footer.html")
         .then(response => response.text())
         .then(data => {
             footerPlaceholder.innerHTML = data;
-        });
+    });
 });
