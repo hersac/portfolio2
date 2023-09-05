fe8ddae98df9ed8cba1bc4e8664dd76a8c18f61b
Heriberto <heriberto@viumail.com>
2023-09-04 19:08:11 -0500

    Se realiza interaccion de fluorenscencia en titulo inicial

--- src/public/assets/js/home.js
+++ src/public/assets/js/home.js
@@ -1,8 +1,14 @@
 const title = document.getElementsByClassName('heri1');
+let validator = 0;
 
 function radiance(){
-
-    title ? title[0].style.filter = 'drop-shadow(0 0 5px var(--primary-color))' : console.log("prueba");
+    if(validator===0){
+        title[0].style.filter = 'drop-shadow(0 0 5px var(--primary-color))';
+        validator = 1;
+    } else{
+        title[0].style.removeProperty('filter');
+        validator = 0;
+    }
 }
 
 setInterval(radiance, 2000);
