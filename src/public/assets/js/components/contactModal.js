cecbc63314f694f345d03e43e0311a27c713ff44
Heriebrto Sanchez <hersac1@gmail.com>
2023-09-08 14:19:41 -0500

    Se agrega funcionalidad de modal para contacto

--- src/public/assets/js/components/contactModal.js
+++ src/public/assets/js/components/contactModal.js
@@ -1,4 +1,19 @@
 fetch("components/contactModal.html").then(response => response.text()).then(data =>{
 	const modal = document.getElementById("contactModal");
+
 	modal.innerHTML = data;
+
+	//Functions modal
+	const emailIcon = document.getElementById('emailIcon');
+	const modalContent = document.getElementsByClassName('modalContent');
+	const backgroundModal = document.getElementsByClassName('backgroundModal');
+
+	emailIcon.addEventListener("click", ()=>{
+		modal.style.display = "flex";
+	});
+
+	backgroundModal[0].addEventListener("click", ()=>{
+		modal.style.display = "none";
+	});
+
 });
