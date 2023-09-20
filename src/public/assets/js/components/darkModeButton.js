72b0facf0f8c474f75e3a2c6df1feeba1591b987
Heriebrto Sanchez <hersac1@gmail.com>
2023-09-20 13:55:31 -0500

    Se implementa boton modo oscuro y su funcionalidad, se implementa persistencia del tema global

--- src/public/assets/js/components/darkModeButton.js
+++ src/public/assets/js/components/darkModeButton.js
@@ -7,4 +7,32 @@ fetch("components/darkModeButton.html")
 
     btnDarkModeContent.innerHTML = data;
 
+    // |-- Functions BTN --|
+
+    const btnLightDark = document.getElementById("btnLightDark");
+    const darkMode = document.getElementById("darkMode");
+    const lightMode = document.getElementById("lightMode");
+    const html = document.documentElement;
+    let count = localStorage.getItem("theme") === "dark" ? 0 : 1;
+
+    // Function to toggle theme
+    function toggleTheme() {
+      if (count === 0) {
+        lightMode.style.display = 'none';
+        darkMode.style.display = 'flex';
+        html.classList.remove("darkMode");
+        localStorage.setItem("theme", "dark");
+        count = 1;
+      } else {
+        lightMode.style.display = 'flex';
+        darkMode.style.display = 'none';
+        html.classList.add("darkMode");
+        localStorage.setItem("theme", "light");
+        count = 0;
+      }
+    }
+    
+    toggleTheme();
+
+    btnLightDark.addEventListener("click", toggleTheme);
   });
